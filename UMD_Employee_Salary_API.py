354dc4ee05b50bbfab0392adcebf49cf7c0d508c
Johann Antisseril <57604319+johann017@users.noreply.github.com>
2021-07-15 16:07:44 -0700

    Update UMD_Employee_Salary_API.py

--- UMD_Employee_Salary_API.py
+++ UMD_Employee_Salary_API.py
@@ -1,4 +1,8 @@
 import requests
+
+#Connects with the Diamondback news API and gets the information in JSON format.
 r = requests.get("https://api.dbknews.com/salary/year/2021?search=&sortby=salary&order=desc&page=1").json()
+
+#Gets the employee's name, salary and their title.
 print("Employee with the highest salary in UMD:")
 print("\tName: " + r.get('data')[0].get('Employee') + "\n\tSalary: " + r.get('data')[0].get('Salary') + "\n\tPosition: " + r.get('data')[0].get("Title"))
