94d6a24f96e030f15142f80781a04a4313e78fbb
Johann Antisseril <57604319+johann017@users.noreply.github.com>
2021-07-15 16:09:52 -0700

    Update News_API.py

--- News_API.py
+++ News_API.py
@@ -1,9 +1,13 @@
 import requests
 
+#Gets the request URL
 url = ('https://newsapi.org/v2/top-headlines?country=us&apiKey=f37c44a1fbb348cea7579fd9ba26e78e')
 
+#Gets the information and converts it into JSON format
 response = requests.get(url)
 r = response.json()
+
+#Prints where the news article is from and prints a description
 for i in range(10):
     print(r.get('articles')[i].get('source').get('name') + ":\n\t" + r.get('articles')[i].get('description') + "\n")
 
