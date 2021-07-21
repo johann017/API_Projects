ae559681fd323455d5010cf2c1ab574ac1253c3d
Johann Antisseril <57604319+johann017@users.noreply.github.com>
2021-07-21 09:09:44 -0700

    Update News_API.py

--- News_API.py
+++ News_API.py
@@ -8,7 +8,12 @@ response = requests.get(url)
 r = response.json()
 
 #Prints where the news article is from and prints a description
-for i in range(10):
-    print(r.get('articles')[i].get('source').get('name') + ":\n\t" + r.get('articles')[i].get('description') + "\n")
+counter = 1
+i = 0
+while counter < 11:
+    if r.get('articles')[i].get('description') != None:
+        print(str(r.get('articles')[i].get('source').get('name')) + ":\n" + str(r.get('articles')[i].get('description')) + "\n")
+        counter += 1
+    i += 1
 
 #Access key: f37c44a1fbb348cea7579fd9ba26e78e
